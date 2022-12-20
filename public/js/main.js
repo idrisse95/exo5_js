c83d67791fa04fe9de62e5ab70ec2c8b63c135a7
55973 <55973@etu.he2b.be>
2022-12-20 10:21:22 +0100

    exo 2

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,8 @@ console.log(tab1.length);
 console.log(tab1[0])
 console.log(tab1[1])
 console.log(tab1[2])
+
+
+let tab2 = ["coding 20","coding 21","coding 22"]
+console.log(tab2.push("idriss"));
+console.log(tab2);
