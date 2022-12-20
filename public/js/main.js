04c5b43fc1b11196788cd0a5fe3b2416c4c05e49
55973 <55973@etu.he2b.be>
2022-12-20 10:27:20 +0100

    krkr

--- public/js/main.js
+++ public/js/main.js
@@ -9,3 +9,10 @@ console.log(tab1[2])
 let tab2 = ["coding 20","coding 21","coding 22"]
 console.log(tab2.push("idriss"));
 console.log(tab2);
+
+
+let tab3 = ["coding 20","coding 21","coding 22"]
+console.log(tab3);
+console.log(tab3.unshift('idriss'));
+console.log(tab3.splice(3,1,'iss'));
+console.log(tab3);
